59279361f4655c8d51d427d4fb1d124ef3239348
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-03-03 20:57:41 +0300

    done

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (tutorials) => {
+  return tutorials.map(titel => titel.split(' ').map(word =>word.charAt(0).toUpperCase()+ word.slice(1)).join(' '))
+};
+
+console.log(titleCased(tutorials));
